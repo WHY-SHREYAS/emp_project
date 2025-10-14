e93c49a3c79db66915bcc8d898cd2c71305aa19d
Shreyas Kale <154947763+WHY-SHREYAS@users.noreply.github.com>
2025-10-14 14:42:50 +0530

    Update home.component.spec.ts

--- employee frontend final/src/app/home/home.component.spec.ts
+++ employee frontend final/src/app/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
