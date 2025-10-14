0a6c7a4f19541f41464f286f49a8ea58ab680196
Shreyas Kale <154947763+WHY-SHREYAS@users.noreply.github.com>
2025-10-14 14:44:08 +0530

    Update show-details.component.spec.ts

--- employee frontend final/src/app/show-details/show-details.component.spec.ts
+++ employee frontend final/src/app/show-details/show-details.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShowDetailsComponent } from './show-details.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 
