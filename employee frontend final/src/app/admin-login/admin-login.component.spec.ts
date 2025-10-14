c5f5720747333c9dedc6d8b791a00afdf7330273
Shreyas Kale <154947763+WHY-SHREYAS@users.noreply.github.com>
2025-10-14 14:43:11 +0530

    Update admin-login.component.spec.ts

--- employee frontend final/src/app/admin-login/admin-login.component.spec.ts
+++ employee frontend final/src/app/admin-login/admin-login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AdminLoginComponent } from './admin-login.component';
 
 describe('AdminLoginComponent', () => {
